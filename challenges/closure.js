8b4c2246287ddc77ae6fd37bcdd671e8378a4168
pvahanian <51976194+pvahanian@users.noreply.github.com>
2020-08-21 11:05:59 -0700

    Sprint Completed with Stretch and Questions

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,18 @@ myFunction();
 
 // Explanation: 
 
+//The scope of "internal" extends to its child (nestedFunction).  Meaning nestedfunction can use varibles like External and Internal but if you were to create a variable inside nestedFunction myFunction would not be able to use it
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(element){
+  let counter=0;
+  for(let x=0;x<=element;x++)
+  {
+    counter+=x;
+  }
+  return counter;
+}
+
